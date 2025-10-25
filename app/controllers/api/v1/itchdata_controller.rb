78f4a1d0bfe817b42827018f6efd881fc087072f
edarasen <edarasen@gmail.com>
2025-10-25 19:03:01 +0800

    controller documentation for channelgroups, channels, comments, itchdata, rawgdata

--- app/controllers/api/v1/itchdata_controller.rb
+++ app/controllers/api/v1/itchdata_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ItchdataController < ApplicationController
+  # GET api/v1/itchdata
+  # Returns : @itchdata data is converted to json
   def index
     @itchdata = Itchdatum.all
     render json: @itchdata
