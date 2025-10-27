68ac2a064de2f6bd674d4fc7abe65e4553618d49
AvelDanielPadilla <aveldanieln1996@gmail.comgit config --global user.name AvelDanielPadillagit config --global user.email aveldanieln1996@gmail.com>
2025-10-27 12:22:56 +0800

    post creation

--- game-forum-front-end/src/App.jsx
+++ game-forum-front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from 'react';
+import { useParams } from "react-router-dom";
 import DataProvider from './context/DataProvider';
 import Login from './pages/Login/Login';
 import Test from './pages/Test/Test'
@@ -15,6 +16,7 @@ import LatestPosts from './pages/LatestPosts/LatestPosts';
 import SearchResults from './pages/SearchResults/SearchResults';
 import MyProfile from './pages/MyProfile/MyProfile';
 import Post from './pages/Post/Post';
+import PostCreate from './pages/PostCreate/PostCreate'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -29,27 +31,56 @@ function App() {
     setIsAuthenticated(false);
   };
 
+  const { channel_id } = useParams();
+
   return (
     <div>
       <DataProvider>
         <BrowserRouter>
           <Routes>
-                          
-            <Route path="/login" element={isAuthenticated ? <Navigate to="/" replace/> : <Login onLogin={handleLogin} /> }/>
+            <Route
+              path="/login"
+              element={
+                isAuthenticated ? (
+                  <Navigate to="/" replace />
+                ) : (
+                  <Login onLogin={handleLogin} />
+                )
+              }
+            />
             <Route path="/">
-              <Route index element={<LandingPage onLogout={handleLogout}/>} />
+              <Route index element={<LandingPage onLogout={handleLogout} />} />
               <Route path="/about" element={<AboutPage />} />
-              <Route path="forums" element={<ForumMain onLogout={handleLogout}/>} />
-              <Route path="channels/:channel_id" element={<Channel onLogout={handleLogout}/>}/>
-              <Route path="permissions-test" element={<Test />}/>
-              <Route path="login-test" element={<Login onLogin={handleLogin} />}/>
+              <Route
+                path="forums"
+                element={<ForumMain onLogout={handleLogout} />}
+              />
+              <Route
+                path="channels/:channel_id"
+                element={<Channel onLogout={handleLogout} />}
+              />
+              <Route path="permissions-test" element={<Test />} />
+              <Route
+                path="login-test"
+                element={<Login onLogin={handleLogin} />}
+              />
               <Route path="my-posts" element={<MyPosts />} />
               <Route path="my-profile" element={<MyProfile />} />
               <Route path="/posts/:id" element={<Post />} />
               <Route path="sign-up" element={<Signup />} />
               <Route path="*" element={<NotFound />} />
-              <Route path="latest" element={<LatestPosts onLogout={handleLogout}/>}/>
-              <Route path="search" element={<SearchResults onLogout={handleLogout}/>}/>
+              <Route
+                path="latest"
+                element={<LatestPosts onLogout={handleLogout} />}
+              />
+              <Route
+                path="search"
+                element={<SearchResults onLogout={handleLogout} />}
+              />
+              <Route
+                path="/channels/:channel_id/posts/new"
+                element={<PostCreate />}
+              />
             </Route>
           </Routes>
         </BrowserRouter>
