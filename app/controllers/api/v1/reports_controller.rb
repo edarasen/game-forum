032c98fbb4bc5fa05278aa9177e5a52a2bacaf32
edarasen <edarasen@gmail.com>
2025-10-31 20:06:45 +0800

    rubocop cleanup

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -26,12 +26,12 @@ class Api::V1::ReportsController < ApplicationController
 
     valid_content = false
     case @report.content_type
-    when 'post'
+    when "post"
       reported_post = Post.find(@report.content_id)
       if !reported_post.nil?
         valid_content = true
       end
-    when 'comment'
+    when "comment"
       reported_comment = Comment.find(@report.content_id)
       if !reported_comment.nil?
         valid_content = true
@@ -50,14 +50,14 @@ class Api::V1::ReportsController < ApplicationController
     @post_reports = Report.posts.where(archive: true).order(:created_at)
     @comment_reports = Report.comments.where(archive: true).order(:created_at)
   end
-  
+
   # PATCH api/v1/reports/archive/:id
   # Uses before_action ensure_admin_moderator before the action proceeds
   # Uses before_action set_report to specify a report
   # Returns : json message: "Archive successful" after Archive action is executed
   def archive
     @report.update(archive: true)
-    render json: {message: 'Archive successful'}, status: :accepted
+    render json: { message: "Archive successful" }, status: :accepted
   end
 
   # DELETE api/v1/reports/:id
@@ -66,13 +66,13 @@ class Api::V1::ReportsController < ApplicationController
   # Returns : json message: "Destroy successful" after destroy action is executed
   def destroy
     @report.destroy!
-    render json: {message: 'Destroy successful'}, status: :accepted
+    render json: { message: "Destroy successful" }, status: :accepted
   end
 
   private
   # Authenticates user and checks role of user
   # Returns : json message: current_user is not an administrator or moderator
-  # OR proceeds to next action 
+  # OR proceeds to next action
   def ensure_admin_moderator
     authenticate_user!
     if !current_user.admin? && !current_user.moderator?
@@ -90,6 +90,6 @@ class Api::V1::ReportsController < ApplicationController
   # @param content_type [Enum:Integer] post or comment
   # @param id [Integer] the post/comment's id
   def report_params
-    params.expect(report: [:content_type, :content_id, :report_reason])
+    params.expect(report: [ :content_type, :content_id, :report_reason ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :reports, only: [:index, :show, :create, :destroy]
+      resources :reports, only: [ :index, :show, :create, :destroy ]
       patch "/reports/archive/:id" => "reports#archive"
 
       namespace :admins do
